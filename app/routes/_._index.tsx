dd9f28ba54d4e581a46bc8e613e440c24844df87
mma29121983 <mma29121983@gmail.com>
2024-02-05 16:06:19 +0000

    adds ssr loading state

--- app/routes/_._index.tsx
+++ app/routes/_._index.tsx
@@ -1,3 +1,8 @@
+import { useNavigation } from "@remix-run/react";
+
 export default function Home() {
-    return "Home page";
+    const nav = useNavigation();
+    const isLoading = nav.state === 'loading';
+    const content = isLoading ? 'Loading...' : "Home page";
+    return content;
 }
